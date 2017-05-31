02f12de9cada14f1343751c54609a48c7b6a9a7e
Dmitriy <pepelyaev.d.a@yandex.ru>
2017-06-01 01:39:43 +0500

    5

--- javas.js
+++ javas.js
@@ -4,21 +4,18 @@ $(document).ready(function () {
 	
 	var comboboxvalue;
 	
-	var container=$('combobox')
-		var comboboxrow=$('<div>').addClass('combobox-row').append(container);
-		$('<span>').addClass('combobox-row-value').append(comboboxrow);
-		$('<span>').addClass('combobox-row-icon ui-icon ui-icon-triangle-1-s').append(comboboxrow);
-		var comboboxlist=$('<div>').addClass('combobox-list').append(container);
-		var comboboxlistul=$('<ul>').append(comboboxlist);
-		$('<li>').text("option 1").append(comboboxlistul);
-		$('<li>').text("option 1").append(comboboxlistul);
-		$('<li>').text("option 1").append(comboboxlistul);
-		$('<li>').text("option 1").append(comboboxlistul);
+	var container=$('#combobox');
 		
-		
-		
-
-	
+		var comboboxrow=$('<div>').addClass('combobox-row').appendTo(container);
+		var comboboxrowvalue=$('<span>').addClass('combobox-row-value').text("Select option").appendTo(comboboxrow);
+		$('<span>').addClass('combobox-row-icon ui-icon ui-icon-triangle-1-s').appendTo(comboboxrow);
+		var comboboxlist=$('<div>').addClass('combobox-list').appendTo(container);
+		var comboboxlistul=$('<ul>').appendTo(comboboxlist);
+		$('<li>').text("option 1").appendTo(comboboxlistul);
+		$('<li>').text("option 2").appendTo(comboboxlistul);
+		$('<li>').text("option 3").appendTo(comboboxlistul);
+		$('<li>').text("option 4").appendTo(comboboxlistul);
+			
 	comboboxrow.on('mousedown', function () {
 		//alert("asd");
 		comboboxlist.toggle('fast');
